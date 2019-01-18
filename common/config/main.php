82cf4308c7dce19c493266bc684087026ee19425
Raman Sobal <sable.lair@gmail.com>
2019-01-18 16:46:53 +0300

    configure pretty urls

--- common/config/main.php
+++ common/config/main.php
@@ -2,12 +2,16 @@
 return [
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
-        '@npm'   => '@vendor/npm-asset',
+        '@npm'   => '@vendor/npm-asset'
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
-            'class' => 'yii\caching\FileCache',
+            'class' => 'yii\caching\FileCache'
         ],
-    ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false
+        ]
+    ]
 ];
