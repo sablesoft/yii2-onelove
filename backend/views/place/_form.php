1c539aef60f4ec80dd03eabaf17c85e0144a6703
Raman Sobal <sable.lair@gmail.com>
2019-01-20 01:57:54 +0300

    update place form

--- backend/views/place/_form.php
+++ backend/views/place/_form.php
@@ -11,18 +11,25 @@ use yii\widgets\ActiveForm;
 <div class="place-form">
 
     <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'address')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'photo')->textInput(['maxlength' => true]) // todo - uploader!! ?>
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="row">
+                <div class="col-sm-6">
+                    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+                </div>
+                <div class="col-sm-6">
+                    <?= $form->field($model, 'phone')->textInput(['maxlength' => true]) ?>
+                </div>
+            </div>
+            <?= $form->field($model, 'address')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-sm-6">
+            <?= $form->field($model, 'photo')->textInput(['maxlength' => true]) // todo - uploader!! ?>
+            <?= $form->field($model, 'is_default')->checkbox(); ?>
+        </div>
+    </div>
 
     <?= $form->field($model, 'map')->textarea(['rows' => 3]); ?>
-
-    <?= $form->field($model, 'phone')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'is_default')->checkbox(); ?>
 
     <div class="form-group">
         <?= Html::submitButton(Yii::t('yii', 'Save'), ['class' => 'btn btn-success']) ?>
