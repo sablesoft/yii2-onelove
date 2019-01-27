01baa2f1edf9897d439056005e37876b8d4aba36
Raman Sobal <sable.lair@gmail.com>
2019-01-28 00:25:22 +0300

    update ask mail links

--- common/mail/ask.php
+++ common/mail/ask.php
@@ -9,7 +9,7 @@ use yii\widgets\DetailView;
 $view = Yii::$app->view;
 $view->title = $model->label;
 \yii\web\YiiAsset::register( $view ); ?>
-<p>
+<p style="font-weight: bold;">
 <?php echo Html::a(
         Yii::t('app', 'Open all asks'),
         Url::to( 'http://admin.onelove.by/ask', true ),
@@ -20,6 +20,13 @@ $view->title = $model->label;
 <?php echo Html::a(
     Yii::t('app', 'View Ask'),
     Url::to( 'http://admin.onelove.by/ask/view?id=' . $model->id, true ),
+    ['class'=>'btn btn-primary']
+);
+?>
+<span> | </span>
+<?php echo Html::a(
+    Yii::t('app', 'Accept Ask'),
+    Url::to( 'http://admin.onelove.by/ask/accept?id=' . $model->id, true ),
     ['class'=>'btn btn-primary']
 );
 ?>
